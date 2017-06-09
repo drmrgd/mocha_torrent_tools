4ea0aa8d2b1526e5a0303b6d6be2c1e8db3b99c9
ionadmin <ionadmin@MCC-S5XL-1>
2017-06-09 09:29:00 -0400

    Fixed regex to handle (the rare) cases where we do not have a typical run name. Code Cleanup

--- generate_clia_case.py
+++ generate_clia_case.py
@@ -6,25 +6,10 @@ import re
 import subprocess
 import shutil
 
-version = '0.1.0_081216'
+version = '1.1.0_060917'
 
-def main():
-    if len(sys.argv) < 3:
-        sys.stderr.write('ERROR: not enough args passed to script!\n')
-        sys.stderr.write('USAGE: {} <run_directory> <clia_case_list.csv>\n'.format(sys.argv[0]))
-        sys.exit(1)
-    rundir,caselist = sys.argv[1:]
-
-    # Make a backup of the current CLIA caselist file before we proceed just in case bad things happen while we're editing it.
-    (path,cfile_name) = os.path.split(caselist)
-    bak_file = '.' + cfile_name + '.bak'
-    shutil.copy2(caselist,os.path.join(path, bak_file))
-
-    run_name = re.match('^Auto_user_(.*?\w{3})_\d+_\d+\/?$',os.path.basename(rundir.rstrip('/'))).group(1)
-    msn_list = get_msn_list(rundir)
-    next_casenum = gen_casenum(caselist)
-    gen_case_list_string(run_name,msn_list,next_casenum,caselist)
-    return
+def usage():
+    sys.stdout.write('USAGE: {} <run_directory> <clia_case_list.csv>\n'.format(os.path.basename(__file__)))
 
 def gen_casenum(caselist):
     with open(caselist) as fh:
@@ -37,7 +22,11 @@ def gen_casenum(caselist):
 
 def get_msn_list(rundir):
     params_file = rundir + '/ion_params_00.json'
-    p = subprocess.Popen(['sampleKeyGen.pl', '-p', '-r', '-f', params_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(
+        ['sampleKeyGen.pl', '-p', '-r', '-f', params_file], 
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
     result,error = p.communicate()
     msn_list = dict([x.split('\t') for x in result.split('\n') if x]) 
     uniq_msn_set = set(v[:-4] for v in msn_list.values() if v.startswith('MSN'))
@@ -51,5 +40,31 @@ def gen_case_list_string(run_name,msns,new_casenum,caselist):
         fh.write(','.join([new_casenum,msn_list,run_name]))
         fh.write('\n')
 
+def get_runname(rundir):
+    try:
+        run_name = re.search('^Auto_user_(.*?\w{3})_\d+_\d+\/?$',os.path.basename(rundir.rstrip('/'))).group(1)
+    except AttributeError:
+        sys.stdout.write("WARN: atypical run name '%s'.  Using full dirname as run name.\n" % rundir)
+        run_name = rundir
+    return run_name
+
 if __name__ == '__main__':
-    main()
+    if (sys.argv[1] == '-h'):
+        usage()
+        sys.exit()
+    elif len(sys.argv) < 3:
+        sys.stderr.write('ERROR: not enough args passed to script!\n')
+        usage()
+        sys.exit(1)
+
+    rundir,caselist = sys.argv[1:]
+
+    # Make a backup of the current CLIA caselist file before we proceed just in case bad things happen while we're 
+    # editing it.
+    (path,cfile_name) = os.path.split(caselist)
+    bak_file = '.' + cfile_name + '.bak'
+    shutil.copy2(caselist,os.path.join(path, bak_file))
+    run_name = get_runname(rundir)
+    msn_list = get_msn_list(rundir)
+    next_casenum = gen_casenum(caselist)
+    gen_case_list_string(run_name,msn_list,next_casenum,caselist)
