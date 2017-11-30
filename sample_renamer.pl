2de3a05118b8bd1e1a606e94cc1a3d3e268c65ef
Dave Sims <misc@lamneth.net>
2017-11-30 16:46:36 -0500

    Updated to handle TagSeq barcodes

--- sample_renamer.pl
+++ sample_renamer.pl
@@ -1,38 +1,47 @@
 #!/usr/bin/perl
 # Use a SampleKey file to rename samples by replacing the "IonXpress_xxx" string with the sample name
 # from the sampleKey.txt file.
-
+# 4/2014 - D Sims
+#####################################################################################################
 use warnings;
 use strict;
-use Getopt::Long;
+use autodie;
+
+use Getopt::Long qw(:config bundling auto_abbrev no_ignore_case);
 use File::Basename;
 use File::Copy;
 use Data::Dump;
 
 my $scriptname = basename($0);
-my $version = "v0.3.0_092415";
+my $version = "3.0.0_092617";
 my $description = <<"EOT";
 Using a sampleKey.txt file, rename files by associating them with the samples indicated in the sampleKey 
 file.  
 EOT
 
 my $usage = <<"EOT";
-USAGE: $scriptname [options] -s <sampleKey.txt> <files_to_rename> 
+USAGE: $scriptname [options] <sampleKey.txt> <files_to_rename> 
+    -k, --keep      Keep the barcode string and just add the sample ID to the name.
+    -s, --string    Add an additional string to the sample name, right after the replaced barcode string
     -n, --no-act    Perform a dry run of the rename process.
-    -s, --sk        Sample key file to use
+    -t, --tagseq    Using new IonCodeTaq barcodes instead of IonXpress barcodes.
     -v, --version   Version information
     -h, --help      Print this help information
 EOT
 
 my $help;
 my $ver_info;
-my $sk_file;
 my $noact;
+my $string;
+my $keep;
+my $tagseq;
 
-GetOptions( "sk=s"          => \$sk_file,
-            "version"     => \$ver_info,
-            "help"        => \$help,
-            "no-act"      => \$noact, 
+GetOptions( "string|s=s"    => \$string,
+            "tagseq|t"      => \$tagseq,
+            "keep|k"        => \$keep,
+            "no-act|n"      => \$noact, 
+            "version|v"     => \$ver_info,
+            "help|h"        => \$help,
     )
         or die $usage;
 
@@ -49,12 +58,6 @@ sub version {
 help if $help;
 version if $ver_info;
 
-if ( ! $sk_file ) {
-    print "ERROR: You must load a samplekey file!\n\n";
-    print $usage;
-    exit 1;
-}
-
 # Make sure enough args passed to script
 if ( scalar( @ARGV ) < 1 ) {
     print "ERROR: No files loaded!\n"; 
@@ -62,35 +65,33 @@ if ( scalar( @ARGV ) < 1 ) {
     exit 1;
 }
 #########------------------------------ END ARG Parsing ---------------------------------#########
-
-#my $samplekey = shift;
-my $samplekey = $sk_file;
+my $samplekey = shift;
 my @input_files = @ARGV;
 
-open( my $sk_fh, "<", $samplekey ) || die "Can't open the sampleKey.txt file for reading: $!";
-
-# Crude check to be sure we have a valid sampleKey file; probably not the best way, but good enough
-#if ( grep { ! /IonXpress_\d+\s+\w+$/ } <$sk_fh> ) {
-if ( my @bad = grep { ! /IonXpress_\d+\s+[-_.\w]+$/ } <$sk_fh> ) {
-    print "ERROR: File '$samplekey' does not appear to be a sampleKey.txt file. Check the file and try again.\n\n";
-    print "Offending lines:\n";
-    print "\t$_" for @bad;
-    print "\n";
-    exit 1;
-}
-seek $sk_fh, 0, 0;
+open( my $sk_fh, "<", $samplekey ); 
 my %sample_hash = map{ split } <$sk_fh>;
+die "ERROR: No barcodes found in sampleKey.txt file!\n" unless grep {$_ =~ /(IonXpress|IonCodeTag)/} keys %sample_hash;
 close $sk_fh;
 
-#dd \%sample_hash;
-#exit;
+if (grep {/IonCode/} keys %sample_hash and ! $tagseq) {
+    print("ERROR: It looks like you have a sampleKey file with IonCodeTag barcodes, but have not selected the '-t' option.\n");
+    exit 1;
+}
 
 for my $file ( @input_files ) {
-    (my $barcode = $file ) =~ s/.*?(IonXpress_\d+).*/$1/;
-    if ( exists $sample_hash{$barcode} ) {
-        (my $new_name = $file) =~ s/IonXpress_\d+/$sample_hash{$barcode}/;
-        ( $noact ) ? (print "$file renamed as $new_name\n") : (move( $file, $new_name) );
-    } else {
-        warn "File '$file' does not have a sample associated with barcode '$barcode' in the sampleKey.txt file.  Skipping...\n";
-    }
+    my ($barcode,$rest);
+    ($tagseq) 
+        ? (($barcode,$rest) = $file =~ /(IonCodeTag_\d{4})(.*)/) 
+        : (($barcode,$rest) = $file =~ /(IonXpress_\d{3})(.*)/);
+
+    unless ( exists $sample_hash{$barcode}) {
+        print "File '$file' does not have a sample associated with barcode '$barcode' in the sampleKey.txt file. ";
+        print "Skipping...\n";
+        next;
+    } 
+    my $new_name = $sample_hash{$barcode};
+    $new_name .= "_${string}"  if $string; # Adding custom string
+    $new_name .= "_${barcode}" if $keep;   # Keeping orig barcode string in.
+    $new_name .= $rest;
+    ( $noact ) ? (print "$file renamed as $new_name\n") : (move( $file, $new_name) );
 }
