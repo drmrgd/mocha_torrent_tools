b65d2ee03516ca00bc3fa04ed66191b1e3943533
ionadmin <ionadmin@FR-W-C135456>
2019-05-20 19:10:15 -0400

    Fixed issue with IonCode samples, and fixed tab split.
    For some reason I wasn't splitting explicitly on a tab, and it was
    negatively affecting the hash.  Also added IonCode barcodes.

--- sample_renamer.pl
+++ sample_renamer.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
-# Use a SampleKey file to rename samples by replacing the "IonXpress_xxx" string with the sample name
-# from the sampleKey.txt file.
+# Use a SampleKey file to rename samples by replacing the "IonXpress_xxx" string
+# with the sample name from the sampleKey.txt file.
 # 4/2014 - D Sims
-#####################################################################################################
+################################################################################
 use warnings;
 use strict;
 use autodie;
@@ -13,18 +13,21 @@ use File::Copy;
 use Data::Dump;
 
 my $scriptname = basename($0);
-my $version = "3.1.022318";
+my $version = "3.2.052019";
 my $description = <<"EOT";
-Using a sampleKey.txt file, rename files by associating them with the samples indicated in the sampleKey 
-file.  
+Using a sampleKey.txt file, rename files by associating them with the samples
+indicated in the sampleKey file.  
 EOT
 
 my $usage = <<"EOT";
 USAGE: $scriptname [options] <sampleKey.txt> <files_to_rename> 
-    -k, --keep      Keep the barcode string and just add the sample ID to the name.
-    -s, --string    Add an additional string to the sample name, right after the replaced barcode string
+    -k, --keep      Keep the barcode string and just add the sample ID to the
+                    name.
+    -s, --string    Add an additional string to the sample name, right after the
+                    replaced barcode string
     -n, --no-act    Perform a dry run of the rename process.
-    -t, --tagseq    Using new IonCodeTag or TagSequencing barcodes instead of IonXpress barcodes.
+    -t, --tagseq    Using new IonCodeTag or TagSequencing barcodes instead of 
+                    IonXpress barcodes.
     -v, --version   Version information
     -h, --help      Print this help information
 EOT
@@ -69,12 +72,15 @@ my $samplekey = shift;
 my @input_files = @ARGV;
 
 open( my $sk_fh, "<", $samplekey ); 
-my %sample_hash = map{ split } <$sk_fh>;
-die "ERROR: No barcodes found in sampleKey.txt file!\n" unless grep {$_ =~ /(IonXpress|IonCodeTag|TagSequencing)/} keys %sample_hash;
+my %sample_hash = map{ chomp; split(/\t/) } <$sk_fh>;
+unless (grep {$_ =~ /(IonXpress|IonCode|TagSequencing)/} keys %sample_hash) {
+    die "ERROR: No barcodes found in sampleKey.txt file!\n" 
+}
 close $sk_fh;
 
 if (grep {/Tag/} keys %sample_hash and ! $tagseq) {
-    print("ERROR: It looks like you have a sampleKey file with IonCodeTag barcodes, but have not selected the '-t' option.\n");
+    print("ERROR: It looks like you have a sampleKey file with IonCodeTag ",
+        "barcodes, but have not selected the '-t' option.\n");
     exit 1;
 }
 
@@ -82,7 +88,8 @@ for my $file ( @input_files ) {
     my ($barcode, $rest) = $file =~ /(.*?_\d+)_(.*)/;
 
     unless ( exists $sample_hash{$barcode}) {
-        print "File '$file' does not have a sample associated with barcode '$barcode' in the sampleKey.txt file. ";
+        print "File '$file' does not have a sample associated with barcode ",
+            "'$barcode' in the sampleKey.txt file. ";
         print "Skipping...\n";
         next;
     } 
@@ -90,5 +97,7 @@ for my $file ( @input_files ) {
     $new_name .= "_${string}"  if $string; # Adding custom string
     $new_name .= "_${barcode}" if $keep;   # Keeping orig barcode string in.
     $new_name .= "_$rest";
-    ( $noact ) ? (print "$file renamed as $new_name\n") : (move( $file, $new_name) );
+    ( $noact ) 
+        ? (print "$file renamed as $new_name\n") 
+        : (move( $file, $new_name) );
 }
